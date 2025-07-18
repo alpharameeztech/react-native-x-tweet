b6be9edee31e4bedc062a9500fec7fe6c4fc9da4
Rameez Israr <rameez@govassist.com>
2025-07-18 19:06:20 +0500

    Add screens

---
+++ app/(screens)/new-tweet.jsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function NewTweetScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>New Tweet Screen</Text>
+    </View>
+  );
+}
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,13 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
-
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { Image } from 'expo-image';
+import { useRouter } from 'expo-router';
+import { Button, Platform, StyleSheet } from 'react-native';
 
 export default function HomeScreen() {
+  const router = useRouter();
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
@@ -19,6 +20,11 @@ export default function HomeScreen() {
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Welcome!</ThemedText>
         <HelloWave />
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <Button title="Go to Profile" onPress={() => router.push('/profile')} />
+        <Button title="View Tweet" onPress={() => router.push('/tweet')} />
+        <Button title="Compose New Tweet" onPress={() => router.push('/new-tweet')} />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
