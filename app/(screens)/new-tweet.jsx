393ceb4e8ad4976a596fc4d1528a03dd14d2c295
Rameez Israr <rameez@govassist.com>
2025-07-28 13:11:07 +0500

    Add new tweet screen

--- app/(screens)/new-tweet.jsx
+++ app/(screens)/new-tweet.jsx
@@ -1,9 +1,41 @@
-import { Text, View } from 'react-native';
-
+import { ThemedText } from '@/components/ThemedText';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
 export default function NewTweetScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>New Tweet Screen</Text>
+    <View style={styles.container}>
+     <View style={styles.tweetButtonContainer}>
+      <ThemedText>
+        Character left: 280
+      </ThemedText>
+      <TouchableOpacity style={styles.tweetButton}>
+        <ThemedText style={styles.tweetButtonText}>
+          Character left: 280
+        </ThemedText>
+      </TouchableOpacity>
+     </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  textColor:{
+    color: 'gray'
+  },
+  ml4:{
+    marginLeft: 16
+  },
+  followButton:{
+    backgroundColor: '#0f1418',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 24
+  },
+  followButtonText: {
+   color: 'white',
+   fontWeight: 'bold'
+  }
+});
