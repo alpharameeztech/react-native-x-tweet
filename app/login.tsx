db9a845715136d38adc0204d047a5f11c9a0cbcb
Rameez Israr <rameez@govassist.com>
2025-08-08 17:14:48 +0500

    Push styling for the login screen

--- app/login.tsx
+++ app/login.tsx
@@ -1,8 +1,9 @@
 import React, { useContext, useState } from 'react';
-import {View, TextInput, Button, SafeAreaView, ActivityIndicator} from 'react-native';
+import {View, TextInput, Button, SafeAreaView, ActivityIndicator, TouchableOpacity} from 'react-native';
 import { AuthContext } from '@/app/(screens)/context/AuthProvider';
 import { ThemedText } from '@/components/ThemedText';
 import { useRouter } from 'expo-router';
+import { StyleSheet } from 'react-native';
 
 export default function LoginScreen() {
     const [email, setEmail] = useState('');
@@ -12,52 +13,107 @@ export default function LoginScreen() {
 
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: '#f0f0f0' }}>
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <ThemedText>Login Screen</ThemedText>
+            <View style={styles.container}>
+                <View style={{ marginTop: 130, width: 260 }}>
+                    <ThemedText>Login Screen</ThemedText>
 
-                <TextInput
-                    style={{
-                        borderWidth: 1,
-                        borderColor: '#ccc',
-                        width: 250,
-                        padding: 10,
-                        marginBottom: 10,
-                        backgroundColor: 'white',
-                    }}
-                    onChangeText={setEmail}
-                    value={email}
-                    placeholder="Email"
-                    placeholderTextColor="gray"
-                    textContentType="emailAddress"
-                    keyboardType="email-address"
-                    autoCapitalize="none"
-                />
+                    <View style={{ marginTop: 40 }}>
+                        {error && <ThemedText style={{ color: 'red' }}>{error}</ThemedText>}
+                        <TextInput
+                            style={[styles.inputBox, styles.mt4]}
+                            onChangeText={setEmail}
+                            value={email}
+                            placeholder="Email"
+                            placeholderTextColor="gray"
+                            textContentType="emailAddress"
+                            keyboardType="email-address"
+                            autoCapitalize="none"
+                        />
+                        <TextInput
+                            style={[styles.inputBox, styles.mt4]}
+                            onChangeText={setPassword}
+                            value={password}
+                            placeholder="Password"
+                            placeholderTextColor="gray"
+                            autoCapitalize="none"
+                            secureTextEntry={true}
+                        />
+                    </View>
 
-                <TextInput
-                    style={{
-                        borderWidth: 1,
-                        borderColor: '#ccc',
-                        width: 250,
-                        padding: 10,
-                        marginBottom: 10,
-                        backgroundColor: 'white',
-                    }}
-                    onChangeText={setPassword}
-                    value={password}
-                    placeholder="Password"
-                    placeholderTextColor="gray"
-                    autoCapitalize="none"
-                    secureTextEntry={true}
-                />
+                    <TouchableOpacity
+                        onPress={() => login(email, password)}
+                        style={[styles.loginButton, styles.mt5]}
+                    >
+                        {isLoading && (
+                            <ActivityIndicator
+                                style={{ marginRight: 18 }}
+                                size="small"
+                                color="white"
+                            />
+                        )}
+                        <ThemedText style={styles.loginButtonText}>Login</ThemedText>
+                    </TouchableOpacity>
 
-                <Button onPress={() => login(email, password)} title="Login" />
-                <Button onPress={() => router.push('/register')} title="Go to Register Screen" />
+                    <View
+                        style={{
+                            flexDirection: 'row',
+                            justifyContent: 'center',
+                            marginTop: 12,
+                        }}
+                    >
+                        <ThemedText style={styles.registerText}>Don't have an account?</ThemedText>
+                        <TouchableOpacity
+                            onPress={() => navigation.navigate('Register Screen')}
+                        >
+                            <ThemedText style={styles.registerTextLink}> Register</ThemedText>
+                        </TouchableOpacity>
+                    </View>
 
-                {error && <ThemedText style={{ color: 'red' }}>{error}</ThemedText>}
-                {isLoading && (
-                    <ActivityIndicator style={{ marginTop: 8 }} size="small" color="gray" />
-                )}
+                </View>
+
             </View>
         </SafeAreaView>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#1d9bf1',
+        alignItems: 'center',
+    },
+    logo: {
+        width: 100,
+        height: 120,
+    },
+    inputBox: {
+        backgroundColor: 'white',
+        borderRadius: 5,
+        padding: 10,
+    },
+    loginButton: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#0084b3',
+        padding: 12,
+        borderRadius: 5,
+    },
+    loginButtonText: {
+        color: 'white',
+    },
+    registerText: {
+        fontSize: 12,
+    },
+    registerTextLink: {
+        fontSize: 12,
+        color: 'white',
+        textDecorationLine: 'underline',
+    },
+    mt4: {
+        marginTop: 16,
+    },
+    mt5: {
+        marginTop: 22,
+    },
+});
