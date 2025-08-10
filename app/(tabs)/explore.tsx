5c25fc69ad8b5f343095a2830aa5f24d426738d8
Rameez Israr <rameez@govassist.com>
2025-08-10 20:35:24 +0500

    Show all the tweets on the explore page

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,110 +1,208 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
-
-import { Collapsible } from '@/components/Collapsible';
-import { ExternalLink } from '@/components/ExternalLink';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import { IconSymbol } from '@/components/ui/IconSymbol';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import EvilIcons from '@expo/vector-icons/EvilIcons';
+
+import { formatDistanceToNowStrict } from 'date-fns';
+import { Image } from 'expo-image';
+import { useLocalSearchParams, useRouter } from 'expo-router';
+import {useContext, useEffect, useRef, useState} from 'react';
+import { ActivityIndicator, FlatList, Platform, StyleSheet, TouchableOpacity, View } from 'react-native';
+import axiosConfig from '../../helpers/axiosConfig';
+import formatDistance from '../../helpers/formatDistanceCustom';
+import RenderItem from "@/components/RenderItem";
 
 export default function TabTwoScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
-      headerImage={
-        <IconSymbol
-          size={310}
-          color="#808080"
-          name="chevron.left.forwardslash.chevron.right"
-          style={styles.headerImage}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Explore</ThemedText>
-      </ThemedView>
-      <ThemedText>This app includes example code to help you get started.</ThemedText>
-      <Collapsible title="File-based routing">
-        <ThemedText>
-          This app has two screens:{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/explore.tsx</ThemedText>
-        </ThemedText>
-        <ThemedText>
-          The layout file in <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{' '}
-          sets up the tab navigator.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/router/introduction">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Android, iOS, and web support">
-        <ThemedText>
-          You can open this project on Android, iOS, and the web. To open the web version, press{' '}
-          <ThemedText type="defaultSemiBold">w</ThemedText> in the terminal running this project.
-        </ThemedText>
-      </Collapsible>
-      <Collapsible title="Images">
-        <ThemedText>
-          For static images, you can use the <ThemedText type="defaultSemiBold">@2x</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to provide files for
-          different screen densities
-        </ThemedText>
-        <Image source={require('@/assets/images/react-logo.png')} style={{ alignSelf: 'center' }} />
-        <ExternalLink href="https://reactnative.dev/docs/images">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Custom fonts">
-        <ThemedText>
-          Open <ThemedText type="defaultSemiBold">app/_layout.tsx</ThemedText> to see how to load{' '}
-          <ThemedText style={{ fontFamily: 'SpaceMono' }}>
-            custom fonts such as this one.
-          </ThemedText>
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/versions/latest/sdk/font">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Light and dark mode components">
-        <ThemedText>
-          This template has light and dark mode support. The{' '}
-          <ThemedText type="defaultSemiBold">useColorScheme()</ThemedText> hook lets you inspect
-          what the user&apos;s current color scheme is, and so you can adjust UI colors accordingly.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/develop/user-interface/color-themes/">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Animations">
-        <ThemedText>
-          This template includes an example of an animated component. The{' '}
-          <ThemedText type="defaultSemiBold">components/HelloWave.tsx</ThemedText> component uses
-          the powerful <ThemedText type="defaultSemiBold">react-native-reanimated</ThemedText>{' '}
-          library to create a waving hand animation.
-        </ThemedText>
-        {Platform.select({
-          ios: (
-            <ThemedText>
-              The <ThemedText type="defaultSemiBold">components/ParallaxScrollView.tsx</ThemedText>{' '}
-              component provides a parallax effect for the header image.
-            </ThemedText>
-          ),
-        })}
-      </Collapsible>
-    </ParallaxScrollView>
-  );
+    const router = useRouter();
+
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [isRefreshing, setIsRefreshing] = useState(false);
+    const [page, setPage] = useState(1);
+    const [isAtEndOfScrolling, setIsAtEndOfScrolling] = useState(false);
+    const flatListRef = useRef(null);
+
+    const { newTweetAdded } = useLocalSearchParams();
+
+    useEffect(() => {
+        getAllTweetsRefresh();
+
+        setTimeout(() => {
+            if (flatListRef.current) {
+                flatListRef.current.scrollToOffset({ offset: 0, animated: true });
+            }
+        }, 100);
+
+    }, [newTweetAdded]);
+
+    useEffect(() =>{
+        getAllTweets()
+    }, [page])
+
+    function getAllTweetsRefresh(){
+        setPage(1);
+        setIsAtEndOfScrolling(false);
+        setIsRefreshing(true);
+
+        axiosConfig.get(`/tweets_all`)
+            .then(response => {
+                setData(response.data.data);
+                setIsLoading(false);
+                setIsRefreshing(false);
+            })
+            .catch(error => {
+                console.log(error);
+                setIsLoading(false);
+                setIsRefreshing(false);
+            })
+    }
+
+    function getAllTweets(){
+
+        axiosConfig.get(`/tweets_all?page=${page}`)
+            .then(response => {
+                if(page == 1){
+                    setData(response.data.data);
+                } else{
+                    setData([...data, ...response.data.data])
+                }
+
+                if(!response.data.next_page_url){
+                    setIsAtEndOfScrolling(true);
+                }
+
+                setIsLoading(false);
+                setIsRefreshing(false);
+            })
+            .catch(error => {
+                console.log(error);
+                setIsLoading(false);
+                setIsRefreshing(false);
+            })
+    }
+
+    function handleRefresh(){
+        setPage(1);
+        setIsAtEndOfScrolling(false);
+        setIsRefreshing(true);
+        getAllTweets();
+    }
+
+    function handleEnd(){
+        setPage(page + 1);
+    }
+
+
+    function goToNewTweet(){
+        router.push('/new-tweet');
+    }
+
+    return (
+        <View style={{ flex: 1, position: 'relative' }}>
+            {isLoading ? (
+                <ActivityIndicator style={{marginTop:10}} size="large" />
+            ):(
+                <FlatList
+                    ref={flatListRef}
+                    data={data}
+                    renderItem={({item}) =>
+                        <>
+                            <RenderItem item={item} />
+                        </>
+                    }
+                    keyExtractor={item => item.id}
+                    ItemSeparatorComponent={()=> <View style={styles.tweetSeparator}></View>}
+                    refreshing={isRefreshing}
+                    onRefresh={handleRefresh}
+                    onEndReached={handleEnd}
+                    onEndReachedThreshold={0}
+                    ListFooterComponent={() => !isAtEndOfScrolling && (
+                        <ActivityIndicator size="large" color="gray" />
+                    )}
+                />
+            )}
+
+            <TouchableOpacity
+                style={styles.floatingButton}
+                onPress={() => goToNewTweet()}
+            >
+                <AntDesign name="plus" size={24} color="white" />
+            </TouchableOpacity>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
-    position: 'absolute',
-  },
-  titleContainer: {
-    flexDirection: 'row',
-    gap: 8,
-  },
+    titleContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 8,
+    },
+    stepContainer: {
+        gap: 8,
+        marginBottom: 8,
+    },
+    reactLogo: {
+        height: 178,
+        width: 290,
+        bottom: 0,
+        left: 0,
+        position: 'absolute',
+    },
+    container:{
+        flex:1, alignItems: 'center', justifyContent: 'center'
+    },
+    tweetContainer:{
+        flexDirection: 'row',
+        paddingHorizontal: 12,
+        paddingVertical: 12
+    },
+    avatar:{
+        width: 40,
+        height: 40,
+        marginRight: 8,
+        borderRadius: 10
+    },
+    flexRow:{
+        flexDirection: 'row'
+    },
+    tweetName:{
+        fontWeight: 'bold',
+        color: '#222222'
+    },
+    tweetHandle:{
+        marginHorizontal: 8,
+        color: 'gray'
+    },
+    tweetContentContainer:{
+        marginTop: 4,
+    },
+    tweetSeparator:{
+        borderBottomWidth: 1,
+        borderBottomColor: '#e5e7eb'
+    },
+    tweetContent:{
+        lineHeight: 20
+    },
+    textGray: {
+        color: 'gray',
+    },
+    tweetEngagement:{
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginTop: 12
+    },
+    ml4:{
+        marginLeft: 16
+    },
+    floatingButton:{
+        width:60,
+        height: 60,
+        borderRadius: 30,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#1d9bf1',
+        position: 'absolute',
+        bottom: 100,
+        right: 12,
+    }
 });
